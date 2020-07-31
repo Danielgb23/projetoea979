bfd4efa486ba010cba00282a5d98b6c9a2fd25a8
Renan Sterle <renanstrle@hotmail.com>
2020-07-30 23:50:06 -0300

    Runway detection improoved.

--- DataSet/Process.py
+++ DataSet/Process.py
@@ -2,46 +2,59 @@ import sys;
 import cv2;
 import numpy as np;
 
-#Adapted from https://stackoverflow.com/questions/39308030/how-do-i-increase-the-contrast-of-an-image-in-python-opencv
-def Contrast(Image, Brightness = 0.0, Contrast = 2.0):
-	return cv2.addWeighted(Image, Contrast, Image, 0.0, 127*(1.0+Brightness-Contrast));
-
 RawImage = cv2.imread(sys.argv[1]);
 
 # 1. Enhance contrast;
-Contrast = Contrast(RawImage, 0, 3);
+Alpha = 3.0;
+Beta = -Alpha*np.mean(cv2.mean(RawImage)[0:3]);
+Contrast = cv2.convertScaleAbs(RawImage, alpha = Alpha, beta = Beta);
 
 # 2. Blur;
-Blur = cv2.bilateralFilter(Contrast, 20, 100, 20);
+sigmaColor = [40, 200];
+sigmaSpace = 10;
+Blur1 = cv2.bilateralFilter(Contrast, d = 0, sigmaColor = sigmaColor[0], sigmaSpace = sigmaSpace, borderType = cv2.BORDER_WRAP);
+Blur2 = cv2.bilateralFilter(Contrast, d = 0, sigmaColor = sigmaColor[1], sigmaSpace = sigmaSpace, borderType = cv2.BORDER_WRAP);
 
 # 3. Canny;
-Edges = cv2.Canny(Blur, 100, 300);
+Edges1 = cv2.Canny(Blur1, 100, 300);
+Edges2 = cv2.Canny(Blur2, 80, 150);
 
-# 4. Dilatation:
-KernelSize = 2;
+# 4. Crop:
+Margin = 5;
+Edges1 = Edges1[Margin:-Margin, Margin:-Margin];
+Edges2 = Edges2[Margin:-Margin, Margin:-Margin];
+
+# 5. Dilatation:
+KernelSize = 3;
 Kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (KernelSize, KernelSize));
-Dilation = cv2.dilate(Edges, Kernel, iterations = 1);
+Dilation1 = cv2.dilate(Edges1, Kernel, iterations = 1);
+Dilation2 = cv2.dilate(Edges2, Kernel, iterations = 1);
 
-Opening = cv2.morphologyEx(Dilation, cv2.MORPH_OPEN, Kernel);
-Closing = cv2.morphologyEx(Opening, cv2.MORPH_CLOSE, Kernel);
+# 6. Hough;
+Lines1 = cv2.HoughLinesP(Dilation1, 1, np.pi/180, threshold = 40, minLineLength = 60, maxLineGap = 5);
+Lines2 = cv2.HoughLinesP(Dilation2, 1, np.pi/180, threshold = 40, minLineLength = 60, maxLineGap = 5);
 
-# 5. Hough;
-Lines = cv2.HoughLinesP(Opening, 1, np.pi/180, threshold = 80, minLineLength = 100, maxLineGap = 5);
+if len(Lines1):
+	for X in Lines1:
+		cv2.line(RawImage, (X[0][0]+Margin, X[0][1]+Margin), (X[0][2]+Margin, X[0][3]+Margin), (255, 0, 255), 1);
+		#print((X[0][0], X[0][1]), (X[0][2], X[0][3]));
+		
+if len(Lines2):
+	for X in Lines2:
+		cv2.line(RawImage, (X[0][0]+Margin, X[0][1]+Margin), (X[0][2]+Margin, X[0][3]+Margin), (0, 255, 0), 1);
+		#print((X[0][0], X[0][1]), (X[0][2], X[0][3]));
 
-for X in Lines:
-	cv2.line(Blur, (X[0][0], X[0][1]), (X[0][2], X[0][3]), (255, 0, 255), 3);
-	print((X[0][0], X[0][1]), (X[0][2], X[0][3]));
-
-# 5. Identification;
-# 6. Modelling;
+# 7. Identification;
+# 8. Modelling;
 
 cv2.imshow("RawImage", RawImage);
 cv2.imshow("Contrast", Contrast);
-cv2.imshow("Blur", Blur);
-cv2.imshow("Edges", Edges);
-cv2.imshow("Dilation", Dilation);
-cv2.imshow("Opening", Opening);
-cv2.imshow("Closing", Closing);
+cv2.imshow("Blur1", Blur1);
+cv2.imshow("Blur2", Blur2);
+cv2.imshow("Dilation1", Dilation1);
+cv2.imshow("Dilation2", Dilation2);
+#cv2.imshow("Opening", Opening);
+#cv2.imshow("Closing", Closing);
 
 cv2.waitKey(0);
 cv2.destroyAllWindows();
